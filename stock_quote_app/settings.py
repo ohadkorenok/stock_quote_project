94c60e6adfba70948e3b12cedfb20bfceb1ffcb3
ohadkoren <ohadkorenok@gmail.com>
2021-01-07 16:26:17 +0200

    add values for dev

--- stock_quote_app/settings.py
+++ stock_quote_app/settings.py
@@ -78,15 +78,24 @@ WSGI_APPLICATION = 'stock_quote_app.wsgi.application'
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
 DATABASES = {
+
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.postgresql',
+    #     'NAME': os.environ.get("POSTGRES_DB"),
+    #     'USER': os.environ.get("POSTGRES_USER"),
+    #     'PASSWORD': os.environ.get("POSTGRES_PASSWORD"),
+    #     'HOST': 'db',
+    #     'PORT': 5432,
+    # }
+    # TODO :: remove before deployment
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.environ.get("POSTGRES_DB"),
-        'USER': os.environ.get("POSTGRES_USER"),
-        'PASSWORD': os.environ.get("POSTGRES_PASSWORD"),
-        'HOST': 'db',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres',
+        'HOST': 'localhost',
         'PORT': 5432,
     }
-    # 'default': env.db("DATABASE_URL", default="postgres:////postgres")
 }
 
 # Password validation
