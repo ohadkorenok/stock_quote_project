19200426905c6dbc6c7c02b8cc14c3cdd3d796e0
ohadkoren <ohadkorenok@gmail.com>
2021-01-08 16:13:23 +0200

    rate_limiter implementation

--- stock_quote_app/settings.py
+++ stock_quote_app/settings.py
@@ -88,15 +88,6 @@ DATABASES = {
         'HOST': 'db',
         'PORT': 5432,
     }
-    # TODO :: remove before deployment
-    # 'default': {
-    #     'ENGINE': 'django.db.backends.postgresql',
-    #     'NAME': 'postgres',
-    #     'USER': 'postgres',
-    #     'PASSWORD': 'postgres',
-    #     'HOST': 'localhost',
-    #     'PORT': 5432,
-    # }
 }
 
 # Password validation
